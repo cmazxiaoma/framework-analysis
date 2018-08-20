c3b88e1134870c0c6b096ffce59e8740dd42727e
cmazxiaoma <1363739582@qq.com>
2018-08-20 10:26:59 +0800

    1.add custom AQS lock

---
+++ src/main/java/com/cmazxiaoma/concurrent/lock/AqsLock.java
@@ -0,0 +1,83 @@
+package com.cmazxiaoma.concurrent.lock;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.AbstractQueuedLongSynchronizer;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+/**
+ * @author cmazxiaoma
+ * @version V1.0
+ * @Description: TODO
+ * @date 2018/8/20 10:05
+ */
+public class AqsLock implements Lock {
+
+    private final Sync sync = new Sync();
+
+    private static class Sync extends AbstractQueuedSynchronizer {
+
+        @Override
+        protected boolean tryAcquire(int arg) {
+            if (compareAndSetState(0, 1)) {
+                setExclusiveOwnerThread(Thread.currentThread());
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        protected boolean tryRelease(int arg) {
+            if (getState() == 0) {
+                throw new IllegalMonitorStateException();
+            }
+            setExclusiveOwnerThread(null);
+            setState(0);
+            return true;
+        }
+
+        @Override
+        protected boolean isHeldExclusively() {
+            return getState() == 1;
+        }
+
+        Condition conditionObject () {
+            return new ConditionObject();
+        }
+    }
+
+    @Override
+    public void lock() {
+        sync.acquire(1);
+    }
+
+    @Override
+    public void lockInterruptibly() throws InterruptedException {
+        sync.acquireInterruptibly(1);
+    }
+
+    @Override
+    public boolean tryLock() {
+        return sync.tryAcquire(1);
+    }
+
+    @Override
+    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+        return sync.tryAcquireNanos(1, unit.toNanos(time));
+    }
+
+    @Override
+    public void unlock() {
+        sync.release(1);
+    }
+
+    @Override
+    public Condition newCondition() {
+        return sync.conditionObject();
+    }
+
+    public boolean isLocked() {
+        return sync.isHeldExclusively();
+    }
+}
