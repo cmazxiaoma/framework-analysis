fdd40c92e378496b70612f2967de5ca304097502
cmazxiaoma <1363739582@qq.com>
2018-11-20 12:48:09 +0800

    add dateformat safely

---
+++ src/main/java/com/cmazxiaoma/concurrent/MyThreadLocal.java
@@ -0,0 +1,35 @@
+package com.cmazxiaoma.concurrent;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author cmazxiaoma
+ * @version V1.0
+ * @Description: TODO
+ * @date 2018/11/20 12:43
+ */
+public class MyThreadLocal {
+
+    public static final ThreadLocal<DateFormat> dateFormatThreadLocal = new ThreadLocal<DateFormat>(){
+        @Override
+        protected DateFormat initialValue() {
+            return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        }
+    };
+
+    public static String format(Date date) {
+        return dateFormatThreadLocal.get().format(date);
+    }
+
+    public static Date parse(String date) throws ParseException {
+        return dateFormatThreadLocal.get().parse(date);
+    }
+
+    public static void main(String[] args) {
+        String date = format(new Date());
+        System.out.println(date);
+    }
+}
